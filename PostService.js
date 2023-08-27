99bfa53e508efe66bd6a23fe4a4d81bc467d8b93
Anton Chekalenko <aston23112001@gmail.com>
2023-08-27 14:06:10 +0300

    added postService

---
+++ PostService.js
@@ -0,0 +1,12 @@
+import Post from "./Post.js";
+
+class PostService{
+
+    async create(post) {
+        const createdPost = await Post.create(post);
+        return createdPost        
+    }
+
+}
+
+export default new PostService()
